3bf5d908751f6b44253e562f615d85fb2745437d
Marcus Vinicius Campos Soares <5ucr4m@gmail.com>
2019-04-06 12:40:44 -0300

    Implementado last login

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,10 +1,16 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class SessionController {
   async store ({ request, auth }) {
     const data = request.only(['email', 'password'])
-
     const token = await auth.attempt(data.email, data.password)
+
+    !!token &&
+      (await User.query()
+        .where('email', data.email)
+        .update({ last_login: new Date() }))
 
     return token
   }
